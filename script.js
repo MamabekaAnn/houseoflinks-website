0dce73c33ac63a3f2797bdcffdedf09baa6fa102
MamabekaAnn <107418604+MamabekaAnn@users.noreply.github.com>
2025-12-12 18:41:30 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-  document.querySelectorAll(".dropdown-btn").forEach(btn => {
-    btn.addEventListener("click", function (e) {
-      e.preventDefault();
-      this.parentElement.classList.toggle("open");
-    });
-  });
